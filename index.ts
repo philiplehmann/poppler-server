f914bfb490f2b6da4a9a5441c4a08cffb6775781
mehmet sayin <mehmet.sayin@steiner.ch>
2024-01-19 18:47:59 +0100

    Revert "feat: pdf to html"
    This reverts commit 9c02949e69962394f560134dfd5b66b501a31a1c.

--- index.ts
+++ index.ts
@@ -1,49 +1,16 @@
-import { spawn } from 'child_process';
-import express from 'express';
-import fs from 'fs';
-import { v4 as uuidv4 } from 'uuid';
-import path from 'path';
+import { spawn } from "child_process";
+import express from "express";
 
-const PORT = process.env.PORT || '5000';
+const PORT = process.env.PORT || "5000";
 
 const app = express();
 
-app.post('/pdf-to-text', async (req, res) => {
-  const pdfToText = spawn('pdftotext', ['-', '-']);
+app.post("/pdf-to-text", async (req, res) => {
+  const pdfToText = spawn("pdftotext", ["-", "-"]);
   req.pipe(pdfToText.stdin);
   pdfToText.stdout.pipe(res);
 });
 
-app.post('/pdf-to-html', async (req, res) => {
-  const tempPdfPath = path.join(__dirname, `${uuidv4()}.pdf`);
-  const tempHtmlPath = path.join(__dirname, `${uuidv4()}.html`);
-
-  const writeStream = fs.createWriteStream(tempPdfPath);
-  req.pipe(writeStream);
-
-  writeStream.on('finish', () => {
-    const pdfToHtml = spawn('pdftohtml', [
-      '-noframes',
-      tempPdfPath,
-      tempHtmlPath,
-    ]);
-
-    pdfToHtml.on('exit', () => {
-      const readStream = fs.createReadStream(tempHtmlPath);
-      readStream.pipe(res);
-
-      readStream.on('end', () => {
-        fs.unlink(tempPdfPath, (err) => {
-          if (err) console.error(`Error deleting temp PDF file: ${err}`);
-        });
-        fs.unlink(tempHtmlPath, (err) => {
-          if (err) console.error(`Error deleting temp HTML file: ${err}`);
-        });
-      });
-    });
-  });
-});
-
 app.listen(Number(PORT), () => {
-  console.log('start poppler server on ', PORT);
+  console.log("start poppler server on ", PORT);
 });
